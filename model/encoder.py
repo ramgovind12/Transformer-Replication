614cfaa086fc0861f0a967d212d2922be2556529
Ram Govind V <ramgovindvi.in@gmail.com>
2025-06-25 21:22:38 +0530

    set up impute embedding and positional encoding

---
+++ model/encoder.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbedding(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self,x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        pe = torch.zeros(seq_len, d_model)
+        position = torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1)
+
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_model / 2)
+
+        # sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term) 
+        # Cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term) 
+        # Add a batch dimension to the positional encoding
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # Add positional encoding to the input
+        x = x + self.pe[:, :x.size(1), :]
+        # Apply dropout
+        return self.dropout(x)
+
