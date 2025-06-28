351e3a457fee3d6be9d42ff691c5f6e5a75d2c39
Ram Govind V <ramgovindvi.in@gmail.com>
2025-06-28 22:39:49 +0530

    layer normalization set up

--- model/encoder.py
+++ model/encoder.py
@@ -39,4 +39,16 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:, :x.size(1), :]
         # Apply dropout
         return self.dropout(x)
+    
+class LayerNOrmalization(nn.Module):
+    def __init__(self, eps:float = 10**-6)-> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
