b101c715b61e08ca9edcf7d1d72c3031cdf0fb45
Ram Govind V <ramgovindvi.in@gmail.com>
2025-07-14 22:11:22 +0530

    added feedforward layer

--- model/encoder.py
+++ model/encoder.py
@@ -51,4 +51,14 @@ class LayerNOrmalization(nn.Module):
         mean = x.mean(dim = -1, keepdim=True)
         std = x.std(dim = -1, keepdim=True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+    
+class FeedForward(nn.Module):
+    def __init__(self, d_model:int, d_ff:int, dropout:float)-> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model,d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff,d_model)
+
+    def forward(self,x):
+        return self.linear2(self.dropout(torch.relu(self.linear1(x))))
 
